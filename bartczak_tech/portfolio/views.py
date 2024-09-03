ff6d48ad387146c7f1b3999c007a5e80a397db9e
Pawel <pawel@bartczak.tech>
2024-09-03 18:36:49 +0100

    Added project list view

--- bartczak_tech/portfolio/views.py
+++ bartczak_tech/portfolio/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 
 from .models import Hobby
+from .models import Project
 
 
 def home(request):
     hobbies = Hobby.objects.all()
+    projects = Project.objects.all()
 
     context = {
         "hobbies_list": hobbies,
+        "projects_list": projects,
     }
 
     return render(request, "portfolio/portfolio.html", context)
